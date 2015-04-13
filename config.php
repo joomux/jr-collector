5ff87618bb557c40971ad7b7c5d367e346a50dde
dcoghill <dcoghill@salesforce.com>
2015-04-13 16:21:07 +1000

    Initial commit with DE population test

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+return array(
+    'appsignature' => 'a63290c6-bd42-4ddd-9278-2130e489e015', 
+	'clientid' => 'wjm7t56gtkrzsfz38nrkfp7a',
+	'clientsecret' => 'JNCpQMMg3aAZ9FSKRMgkXvnu',
+	'defaultwsdl' => 'https://webservice.exacttarget.com/etframework.wsdl'//,
+    //'xmlloc' => '/some/path/to/cache/ExactTargetWSDL.xml',
+);
