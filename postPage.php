3254a6d703c3d3ed00f220ec430ed00c97f8666c
dcoghill <dcoghill@dcoghill-ltm.sfdcgomobile.salesforce.com>
2015-04-16 15:46:16 +1000

    Tidied up postPage

--- postPage.php
+++ postPage.php
@@ -1,10 +1,3 @@
-<?php
-
-foreach($_COOKIE as $key=>$val) {
-    var_dump($key.': '.$val);
-}
-
-?>
 <!doctype html>
 <html lang="en">
     <head><title>Tell us what you love</title>
@@ -34,7 +27,7 @@ foreach($_COOKIE as $key=>$val) {
                 }
                 
                 ?>
-                <h1>Hi, <?php print $_SESSION['FirstName'] ?>!</h1><p class="bg-success"><?php print $message; ?>.</p><p>Tell us what you love eating...</p>
+                <h1>Hi, <?php print $_COOKIE['FirstName'] ?>!</h1><p class="bg-success"><?php print $message; ?>.</p><p>Tell us what you love eating...</p>
                 <div class="row">
                     <div class="col-xs-4">
                         <img src="assets/images/hawaiian.png" alt="Hawaiian">
