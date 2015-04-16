6c0d05c6a99b43727b24bab3068f8d753da52a7d
dcoghill <dcoghill@dcoghill-ltm.sfdcgomobile.salesforce.com>
2015-04-16 12:37:36 +1000

    Checking boolean problems

--- populateDE.php
+++ populateDE.php
@@ -30,8 +30,8 @@ require('fuelsdk/ET_Client.php');
 
 foreach($newRowArray as $key) {
     if (isset($_POST[$key])) {
-        if (isint($_POST[$key])) {
-            $_POST[$key] = int($_POST[$key])
+        if (is_int($_POST[$key])) {
+            $_POST[$key] = int($_POST[$key]);
         }
         $newRowArray[$key] = $_POST[$key];
     }
